73c61612e84ba1ae43ceb21c93b9f897d1da793b
Himanshu Jain <nhimanshujain@gmail.com>
2020-06-08 21:12:11 +0530

    Keyframes Extracted and Transcript Cleaned

--- backend/keyframes.py
+++ backend/keyframes.py
@@ -1,10 +1,18 @@
-# Input : Youtube URL (Ex : https://www.youtube.com/watch?v=8HyCNIVRbSU)
+# Input : YouTube URL (Ex : https://www.youtube.com/watch?v=8HyCNIVRbSU)
 # Output : Extracted Frames from the video in the 'out' folder
-# Time of execution : Around 5-7 min
+# Time of execution : Around 5-6 min
 
+'''
+Parameters/Thresholds:
+1) Number of keywords : n
+2) Image Similarity : dis_threshold (Default : 20)
+3) Non-Text Image : text_threshold (Default : 50) -> Len of the text
+4) Jumping by 5000 ms to capture next frame
+'''
+
+# Import modules
 from youtube_transcription import youtube_transcribe
 from keywords_extractor import get_keywords
-from summary_generator import summary
 import io
 
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -13,32 +21,46 @@ import os
 import pafy
 import youtube_dl
 import numpy as np
+import requests
+import imageio
+from PIL import Image
+import pytesseract
+
+# Path to your tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'G:\himanshu\Tesseract-OCR\tesseract.exe'
+
+def detect_text(img):
+    text = pytesseract.image_to_string(img,lang='eng')
+    return text
 
 def frames(u,s,e):    
-    p_frame_thresh = 1300000
     url = u
     vPafy = pafy.new(url)
     play = vPafy.getbest()
     capture = cv2.VideoCapture(play.url)
-    start = s  #1s
-    end = e #3s
+    
+#   capture = cv2.VideoCapture("video1.mp4")   
+
     _,frame = capture.read()
-    prev_frame = frame
-    num = 0
+    start = s
+    end = e
+    start = start + 250
+    global count
     while(start <= end):
+        
         capture.set(cv2.CAP_PROP_POS_MSEC,start)
         _,frame = capture.read()
+        
         if frame is None:
             start = start+250
             continue
-        diff = cv2.absdiff(frame, prev_frame)
-        non_zero_count = np.count_nonzero(diff)
-        if non_zero_count > p_frame_thresh:
-            cv2.imwrite("out/image"+str(start)+".jpg", frame)
-            num+=1
-        start = start+250
+            
+        cv2.imwrite("out/image"+str(count)+".jpg", frame)
+        count+=1
+        
+        start = start+5000
         prev_frame = frame
-    if num == 0: cv2.imwrite("out/image"+str(start)+".jpg", frame)
+        
     capture.release()
 
 
@@ -83,10 +105,21 @@ if __name__ == '__main__':
     number_of_transciptions,transcripts = youtube_transcribe(url)
 
     if number_of_transciptions:
-        with io.open("transcript.txt", "w", encoding="utf-8") as f:
-            text = transcripts[0]
-            f.write(transcripts[0])
-            print("Transcription Done!!")
+        with open("transcript.txt","w") as f:
+        
+                while number_of_transciptions > 0:
+                    if '.' in transcripts[number_of_transciptions-1]:
+                        text = transcripts[number_of_transciptions-1]
+                        f.write(transcripts[number_of_transciptions-1])
+                        break
+                
+                    if number_of_transciptions == 1:
+                        text = transcripts[0]
+                        f.write(transcripts[0])
+                        
+                    number_of_transciptions-=1
+                    
+        print("Transcription Done!!")
     else:
         print("No Transcript Available")
 
@@ -98,8 +131,44 @@ if __name__ == '__main__':
     transcript = YouTubeTranscriptApi.get_transcript(urlID)
     
     os.mkdir('out') 
-    
+
+    # Process Every Keyword
+    count=0
     for key in range(len(keywords)):
         search = keywords[key].split()
         start,end = start_end(transcript,search)
         frames(url,start,end)
+        
+            
+    # Remove non-text images
+    print("Removing non-text images")
+    text_threshold = 50
+    files = os.listdir(r"out")
+    for i in files:
+        res = detect_text('out/'+i)
+        if res == '' or len(res) < text_threshold :
+            os.remove('out/'+i)
+            
+            
+    # Remove Similar Images
+    print("Removing Similar Images")
+    dis_threshold = 20
+    files = os.listdir(r"out")
+    i=0
+    while i<len(files)-1:
+        r = requests.post(
+            "https://api.deepai.org/api/image-similarity",
+            files={
+                'image1': open('out/'+files[i], 'rb'),
+                'image2': open('out/'+files[i+1], 'rb'),
+            },
+            headers={'api-key': '08dd8c70-ad69-43aa-bd1d-01de53d8ebd6'}
+        )
+        res = r.json()
+        
+        dis = res['output']['distance']
+        if dis >= dis_threshold:
+            i+=2
+        else:
+            os.remove('out/'+files[i])
+            i+=1
--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
 from youtube_transcription import youtube_transcribe
 from keywords_extractor import get_keywords
 from summary_generator import summary
+from clean_transcript import add_punctuations,correct_mistakes
 import io
 
 if __name__ == '__main__':
-    # Transcription
+    # Transcription and Cleaning
     url = input("Enter the URL = ")
 
     number_of_transciptions,transcripts = youtube_transcribe(url)
 
     if number_of_transciptions:
         with open("transcript.txt","w") as f:
-        
                 while number_of_transciptions > 0:
                     if '.' in transcripts[number_of_transciptions-1]:
                         text = transcripts[number_of_transciptions-1]
@@ -26,14 +20,18 @@ if __name__ == '__main__':
                 
                     if number_of_transciptions == 1:
                         text = transcripts[0]
+                        text=add_punctuations(text,punct_model)
+                        text=correct_mistakes(text,lang_model)
                         f.write(transcripts[0])
                         
                     number_of_transciptions-=1
                     
-        print("Transcription Done!!")
+        print("Transcription and Cleaing Done!!")
     else:
         print("No Transcript Available")
-
+    
+    print(text)
+    
     # Keywords Extractor
     keywords=get_keywords(text,15)
     print('\nKeywords:\n',keywords)
@@ -42,10 +40,3 @@ if __name__ == '__main__':
     percentage=int(input("Enter the percentage of information in text you want as summary : "))
     result = summary(text,percentage)
     print('\nSummary:\n',result)
-
-
-# In[ ]:
-
-
-
-
