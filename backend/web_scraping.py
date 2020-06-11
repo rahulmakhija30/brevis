3b6c1f9f4b1bdb6aad57a8b16901acc52a91c128
aniketaayush <aniketaayush5@gmail.com>
2020-06-11 16:14:47 +0530

    Changed the youtube scraping method and structure of JSON file generated

--- backend/web_scraping.py
+++ backend/web_scraping.py
@@ -2,27 +2,23 @@
 import json
 from google_scrape import google_scrapper
 from youtube_scrape import youtube_scrapper
-def web_scrape(keywords,google_res=3,youtube_res=3):
-  google_result={}
-  youtube_result={}
+def web_scrape(keywords,google_res=2,youtube_res=2):
+  google_result=[]
+  youtube_result=[]
   for w in keywords:
-    google_result[w]=google_scrapper(w,google_res)
-    youtube_result[w]=youtube_scrapper(w,youtube_res)
-  #return google_result,youtube_result
-  write_json(google_result,youtube_result)
+    google_scrapper(w,google_result,google_res)
+    youtube_scrapper(w,youtube_result,youtube_res)
+  scrape_result=dict()
+  scrape_result["google"]=google_result
+  scrape_result["youtube"]=youtube_result
+  write_json(scrape_result)
 
-def write_json(google_result,youtube_result):
-    #For Google results
-    out_file_google=open("google_results.json","w")
-    json.dump(google_result,out_file_google,indent=4)
-    out_file_google.close()
-    #For Youtube Results
-    out_file_youtube=open("youtube_results.json","w")
-    json.dump(youtube_result,out_file_youtube,indent=4)
-    out_file_youtube.close()
+def write_json(scrape_result):
+    #For Scrape results
+    out_file=open("scrape_results.json","w")
+    json.dump(scrape_result,out_file,indent=4)
+    out_file.close()
 
-##if __name__== "__main__":
-##    keywords=["Artificial Intelligence","Machine Learning"]
-##    web_scrape(keywords)
-
-
+if __name__== "__main__":
+    keywords=["Artificial Intelligence","Machine Learning","Dynamic Programming"]
+    web_scrape(keywords)
--- backend/youtube_scrape.py
+++ backend/youtube_scrape.py
@@ -1,14 +1,73 @@
-from youtube_search import YoutubeSearch
-def youtube_scrapper(query,search_results=3):
-  results = YoutubeSearch(query, max_results=search_results).to_dict()
-  res={}
-  for d in results:
-    res[d["title"]]="www.youtube.com"+d["link"]
-  return res
+#Importing Libraries
+import urllib
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import re
+
+#Function to scrape results from Google Search
+def youtube_scrapper(query,youtube_result,number_results=2):
+  query = urllib.parse.quote_plus(query) # Format into URL encoding
+  ua = UserAgent()
+  assert isinstance(query, str) #Search term must be a string
+  assert isinstance(number_results, int) #Number of results must be an integer
+  escaped_search_term = query.replace(' ', '+')
+  google_url = "https://www.google.com/search?q={}&num={}".format(query+"+site:youtube.com",number_results+1)
+  response = requests.get(google_url, {"User-Agent": ua.random})
+  soup = BeautifulSoup(response.text, "html.parser")
+  result_div = soup.find_all('div', attrs = {'class': 'ZINbbc'})
+  links = []
+  titles = []
+  for r in result_div:
+    # Checks if each element is present, else, raise exception
+    try:
+        link = r.find('a', href = True)
+        title = r.find('div', attrs={'class':'vvjwJb'}).get_text()
+        
+        # Check to make sure everything is present before appending
+        if link != '' and title != '': 
+            links.append(link['href'])
+            titles.append(title)
+            if(len(links)==number_results):
+              break
+    # Next loop if one element is not present
+    except:
+        continue
+  links=clean_results(links,titles)
+  for i in range(0,number_results):
+    if(links[i].find("watch")!=-1):
+      links[i]=links[i].replace("%3F","?")
+      links[i]=links[i].replace("%3D","=")
+    else:
+      continue
+  for i in range(0,number_results):
+    d=dict()
+    d["title"]=titles[i]
+    d["linktopage"]=links[i]
+    youtube_result.append(d)
+
+#Function to remove useless results and clean the links
+def clean_results(links,titles):
+  to_remove = []
+  clean_links = []
+  for i, l in enumerate(links):
+    clean = re.search('\/url\?q\=(.*)\&sa',l)
+
+    # Anything that doesn't fit the above pattern will be removed
+    if clean is None:
+        to_remove.append(i)
+        continue
+    clean_links.append(clean.group(1))
+
+# Remove the corresponding titles
+  for x in to_remove:
+    del titles[x]
+  return clean_links
 
 #Driver Code
-if __name__ == "__main__":
+if __name__=="__main__":
   query=input("Enter Query : ")
-  search_results=int(input("Enter number of results to be fetched : "))
-  res=youtube_scrapper(query,search_results)
+  number_results=int(input("Enter number of results to be fetched : "))
+  res=[]
+  youtube_scrapper(query,res,number_results)
   print(res)
