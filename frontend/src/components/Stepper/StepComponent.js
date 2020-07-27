f64fd6a21eb69ff147b34ad0de4f5e28c73ece58
Mayank1403 <mayank1403@gmail.com>
2020-07-27 12:52:07 +0530

    axios error solved

--- frontend/src/components/Stepper/StepComponent.js
+++ frontend/src/components/Stepper/StepComponent.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import Step1 from './Step1'
 import Step2 from './Step2'
 import Step3 from './Step3'
-import axios from 'axios';
+
 
 
 const QontoConnector = withStyles({
